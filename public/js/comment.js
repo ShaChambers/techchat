e36571c8037a39b1b6821d16e521e613eb64d32a
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-23 12:24:14 -0500

    create new post works

---
+++ public/js/comment.js
@@ -0,0 +1,32 @@
+// Create fetch request for comment POST route
+const submitBtn = document.getElementById("submit-btn");
+
+const clickHandler = async (event) => {
+  event.preventDefault();
+
+  // Split url at every /
+  const url = location.href.split("/");
+  // Grab last index of array (i.e. post number)
+  const post_id = url[url.length - 1];
+  const comment_text = document.querySelector("#comment-input").value.trim();
+
+  if (comment_text) {
+    const response = await fetch(`/api/comments`, {
+      method: "POST",
+      body: JSON.stringify({ comment_text, post_id }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    console.log(result);
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert("Failed to create comment");
+    }
+  }
+};
+
+submitBtn.addEventListener("submit", clickHandler);
---
+++ public/js/updatepost.js
@@ -0,0 +1,33 @@
+// Create fetch request for post UPDATE route
+const updateBtnHandler = async (event) => {
+  event.preventDefault();
+  console.log("Listening");
+
+  const title = document.querySelector("#post-title").value.trim();
+  const content = document.querySelector("#post-content").value.trim();
+  console.log(event.target);
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+    console.log(id);
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ title, content }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+    console.log(result);
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to update post");
+    }
+  }
+};
+
+document
+  .querySelector(".update-post-form")
+  .addEventListener("submit", updateBtnHandler);
